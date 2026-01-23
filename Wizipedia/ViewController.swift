39502f95b7d396c1a86fa06bdad11de93dce2d0e
Facundo <faquidg@hotmail.com>
2026-01-23 16:07:28 +0100

    Initial Commit

---
+++ Wizipedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wizipedia
+//
+//  Created by Tardes on 23/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
