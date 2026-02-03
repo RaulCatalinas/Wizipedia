51eac30e8d5913bd711a45aafe4779ff3f1af0b2
Facundo <faquidg@hotmail.com>
2026-02-03 19:28:54 +0100

    feat: add release date to movie details

--- Wizipedia/Controllers/MovieDetailsViewController.swift
+++ Wizipedia/Controllers/MovieDetailsViewController.swift
@@ -9,9 +9,10 @@ import ExpandableLabel
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
-    @IBOutlet weak var movieTitle: UILabel!
+
+    @IBOutlet weak var movieName: UILabel!
     @IBOutlet weak var movieSummary: ExpandableLabel!
-    // @IBOutlet weak var movieReleaseDate: UILabel!
+    @IBOutlet weak var movieReleaseDate: UILabel!
     @IBOutlet weak var moviePoster: UIImageView!
 
     var movieID: String!
@@ -27,9 +28,12 @@ class MovieDetailsViewController: UIViewController {
 
             DispatchQueue.main.async {
                 self.moviePoster.loadFromInternet(url: self.movie.poster)
-                self.movieTitle.text = self.movie.title
-                self.movieTitle.sizeToFit()
+                self.movieName.text = self.movie.title
+                self.movieName.sizeToFit()
                 self.movieSummary.text = self.movie.summary
+                self.movieReleaseDate.text =
+                    "Release Date: \(self.movie.releaseDate)"
+                self.movieReleaseDate.sizeToFit()
             }
         }
     }
